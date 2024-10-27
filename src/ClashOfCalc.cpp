c0b83405dde8389a3fe5f731069632d0d309a883
siddhartha boyina <sidboyina@gmail.com>
2024-10-27 18:46:21 +0530

    Add project files.

---
+++ src/ClashOfCalc.cpp
@@ -0,0 +1,13 @@
+﻿// ClashOfCalc.cpp : Defines the entry point for the application.
+//
+
+#include "ClashOfCalc.h"
+#include "StateMachine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
